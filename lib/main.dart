9e1c704a1703dac5c3e602fac7afb172f4227645
sanaderali <94048007+sanaderali@users.noreply.github.com>
2021-11-20 10:56:32 +0500

    day3 complted

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/pages/home_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return MaterialApp(
+      home:HomePage;
+    );
+  }
+}
